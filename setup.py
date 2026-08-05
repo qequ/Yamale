aa969750e54ea2fad8527b0c6fb9292f14ace7f2
Alvaro Frias <alvaro.frias@eclypsium.com>
2026-08-05 16:28:38 -0300

    Exclude tests from built distributions

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     license="MIT",
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*"]),
     include_package_data=True,
     install_requires=["pyyaml"],
     extras_require={"ruamel": ["ruamel.yaml"]},
